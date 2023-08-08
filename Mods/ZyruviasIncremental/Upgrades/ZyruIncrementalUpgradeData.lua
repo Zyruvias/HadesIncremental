c46cc530c69b06b3b7d6da833f4a3a9f200aac1d
Zyruvias <zerianledrias@gmail.com>
2023-08-08 07:33:17 -0400

    slight restructure

--- Mods/ZyruviasIncremental/Upgrades/ZyruIncrementalUpgradeData.lua
+++ Mods/ZyruviasIncremental/Upgrades/ZyruIncrementalUpgradeData.lua
@@ -1266,31 +1266,7 @@ ModUtil.LoadOnce(function ( )
     -- })
 end)
 
--- Mirror Upgrads
--- TODO: should this be game state or is local generation fine? mid-run quitiout shenanigans
--- TODO: fix multiple / not-enough applications, LuaUpgrade false maybe?
-local mirrorUpgradesToDuplicate = {}
-function Z.ApplyBothMirrorSidesForUpgrade(upgradeNames)
-    ConcatTableValues(mirrorUpgradesToDuplicate, upgradeNames)
-    for i, upgrade in ipairs(upgradeNames) do
-        DebugPrint { Text = "Attempting to apply " .. upgrade }
-        if GameState.MetaUpgrades[upgrade] == 0 and GameState.MetaUpgrades[upgrade] ~= GameState.MetaUpgradeState[upgrade] then
-            GameState.MetaUpgrades[upgrade] = GameState.MetaUpgradeState[upgrade]
-            DebugPrint { Text = "Applying " .. upgrade }
-            -- TODO: apply specific upgrades only ones, use loop in ApplyMetaUpgrades only
-            ApplyMetaUpgrades( CurrentRun.Hero, true )
-        end
-    end
-end
 
-ModUtil.LoadOnce(function ()
-    ModUtil.Path.Wrap("IsMetaUpgradeActive", function (baseFunc, upgradeName)
-        if Contains(mirrorUpgradesToDuplicate, upgradeName) then
-            return true
-        end
-        return baseFunc(upgradeName)
-    end, Z)
-end)
 
 -- PoseidonAlternatingTidesUpgrade
 -- OnEffectApply{
