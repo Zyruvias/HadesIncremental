9dde968625885d29631644e1433296710004c5ac
Zyruvias <zerianledrias@gmail.com>
2024-04-15 20:35:11 -0400

    log

--- Mods/ZyruviasIncremental/UI/ZyruIncrementalUI.lua
+++ Mods/ZyruviasIncremental/UI/ZyruIncrementalUI.lua
@@ -2090,3 +2090,93 @@ function ShowGodUpgradeScreen(screen, button)
     }
     ZyruIncremental.RenderComponents(screen, componentsToRender)
 end
+
+-- 1.1.0 EXP stuff
+ModUtil.Path.Wrap("OpenShrineUpgradeMenu", function (base, args)
+    -- base calls "HandleScreenInput" unthreaded which blocks our function
+    thread(base, args)
+
+    
+	local screen = ScreenAnchors.ShrineScreen
+    local components = screen.Components
+    ZyruIncremental.CreateOrUpdateComponent(screen, {
+        Type = "Text",
+        SubType = "Note",
+        FieldName = "ExpCounter",
+        Args = {
+            Text = "Exp Multiplier: x" .. string.format("%.2f", ZyruIncremental.ComputeShrinePactExperienceMultiplier()),
+            Parent = "ShopBackground",
+            Group = "Combat_Menu",
+            Font = "AlegreyaSansSCExtraBold",
+            FontSize = 24,
+            OffsetY = -345,
+            Color = Color.White,
+            OffsetX = 200,
+        }
+    })
+end, ZyruIncremental)
+
+local shrineExpFactorTable = {
+	EnemyDamageShrineUpgrade = 1.05,
+	HealingReductionShrineUpgrade = 1.05,
+	ShopPricesShrineUpgrade = 1.1,
+	EnemyCountShrineUpgrade = 1.02,
+	BossDifficultyShrineUpgrade = 1.1,
+
+	EnemyHealthShrineUpgrade = 1.05,
+	EnemyEliteShrineUpgrade = 1.15,
+	MinibossCountShrineUpgrade = 1.1,
+	ForceSellShrineUpgrade = 1.2,
+	EnemySpeedShrineUpgrade = 1.1,
+
+	TrapDamageShrineUpgrade = 1.05,
+	MetaUpgradeStrikeThroughShrineUpgrade = 1.2,
+	EnemyShieldShrineUpgrade = 1.02,
+	ReducedLootChoicesShrineUpgrade = 1.25,
+	BiomeSpeedShrineUpgrade = 1.05,
+	NoInvulnerabilityShrineUpgrade = 1,
+}
+function ZyruIncremental.ComputeShrinePactExperienceMultiplier(args)
+    if args.Cached and ZyruIncremental.ShrinePactExperienceMultiplierCache ~= nil then
+        -- DebugPrint { Text = "returning cached shrine mult: " .. tostring(ZyruIncremental.ShrinePactExperienceMultiplierCache)}
+        return ZyruIncremental.ShrinePactExperienceMultiplierCache
+    end
+    local value = 1 -- multValue
+    local additiveValue = 1
+    local mixedValue = 1
+    for i, pactConditionName in ipairs(ShrineUpgradeOrder) do
+		local upgradeData = MetaUpgradeData[pactConditionName]
+        local expFactor = shrineExpFactorTable[pactConditionName]
+        local pactConditionCount = GetNumMetaUpgrades( pactConditionName ) or 0
+        if pactConditionCount > 0 then
+        
+            value = value * math.pow(expFactor, pactConditionCount)
+            additiveValue = additiveValue + (expFactor - 1) * pactConditionCount
+            mixedValue = mixedValue * ((expFactor - 1) * pactConditionCount + 1)
+        end
+
+        
+    end
+    -- DebugPrint { Text = "Mult: " .. tostring(value) ..  ", Mixed: " .. tostring(mixedValue) .. ", Additive: " .. tostring(additiveValue)}
+    ZyruIncremental.ShrinePactExperienceMultiplierCache = value
+    return value
+end
+function ZyruIncremental.UpdateShrineExperienceMultiplier(screen, button)
+
+    ZyruIncremental.CreateOrUpdateComponent(screen, {
+        Type = "Text",
+        SubType = "Note", 
+        FieldName = "ExpCounter",
+        Args = {
+            Text = "Exp Multiplier: x" .. string.format("%.2f", ZyruIncremental.ComputeShrinePactExperienceMultiplier()),
+        }
+    })
+end
+
+ModUtil.Path.Wrap("HandleMetaUpgradeInput", function (base, screen, button)
+
+    base(screen, button)
+
+    ZyruIncremental.UpdateShrineExperienceMultiplier(screen, button)
+
+end, ZyruIncremental)
