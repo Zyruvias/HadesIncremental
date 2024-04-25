00c296c15d02d0545a2ad8bd3f43b60ad0878b65
Zyruvias <zerianledrias@gmail.com>
2024-04-25 08:50:19 -0400

    add experience

--- Mods/ZyruviasIncremental/Tracking/Experience.lua
+++ Mods/ZyruviasIncremental/Tracking/Experience.lua
@@ -1,22 +1,20 @@
 local shrineExpFactorTable = {
-	EnemyDamageShrineUpgrade = 1.05,
-	HealingReductionShrineUpgrade = 1.05,
-	ShopPricesShrineUpgrade = 1.1,
-	EnemyCountShrineUpgrade = 1.02,
-	BossDifficultyShrineUpgrade = 1.1,
-
-	EnemyHealthShrineUpgrade = 1.05,
-	EnemyEliteShrineUpgrade = 1.15,
-	MinibossCountShrineUpgrade = 1.1,
-	ForceSellShrineUpgrade = 1.2,
-	EnemySpeedShrineUpgrade = 1.1,
-
-	TrapDamageShrineUpgrade = 1.05,
-	MetaUpgradeStrikeThroughShrineUpgrade = 1.2,
-	EnemyShieldShrineUpgrade = 1.02,
-	ReducedLootChoicesShrineUpgrade = 1.25,
-	BiomeSpeedShrineUpgrade = 1.05,
-	NoInvulnerabilityShrineUpgrade = 1,
+    EnemyDamageShrineUpgrade = 1.05,
+    HealingReductionShrineUpgrade = 1.05,
+    ShopPricesShrineUpgrade = 1.1,
+    EnemyCountShrineUpgrade = 1.02,
+    BossDifficultyShrineUpgrade = 1.1,
+    EnemyHealthShrineUpgrade = 1.05,
+    EnemyEliteShrineUpgrade = 1.15,
+    MinibossCountShrineUpgrade = 1.1,
+    ForceSellShrineUpgrade = 1.2,
+    EnemySpeedShrineUpgrade = 1.1,
+    TrapDamageShrineUpgrade = 1.05,
+    MetaUpgradeStrikeThroughShrineUpgrade = 1.2,
+    EnemyShieldShrineUpgrade = 1.02,
+    ReducedLootChoicesShrineUpgrade = 1.25,
+    BiomeSpeedShrineUpgrade = 1.05,
+    NoInvulnerabilityShrineUpgrade = 1
 }
 
 function ZyruIncremental.ComputeShrinePactExperienceMultiplier(args)
@@ -29,19 +27,48 @@ function ZyruIncremental.ComputeShrinePactExperienceMultiplier(args)
     local additiveValue = 1
     local mixedValue = 1
     for i, pactConditionName in ipairs(ShrineUpgradeOrder) do
-		local upgradeData = MetaUpgradeData[pactConditionName]
+        local upgradeData = MetaUpgradeData[pactConditionName]
         local expFactor = shrineExpFactorTable[pactConditionName]
-        local pactConditionCount = GetNumMetaUpgrades( pactConditionName ) or 0
+        local pactConditionCount = GetNumMetaUpgrades(pactConditionName) or 0
         if pactConditionCount > 0 then
-        
             value = value * math.pow(expFactor, pactConditionCount)
             additiveValue = additiveValue + (expFactor - 1) * pactConditionCount
             mixedValue = mixedValue * ((expFactor - 1) * pactConditionCount + 1)
         end
-
-        
     end
     -- DebugPrint { Text = "Mult: " .. tostring(value) ..  ", Mixed: " .. tostring(mixedValue) .. ", Additive: " .. tostring(additiveValue)}
     ZyruIncremental.ShrinePactExperienceMultiplierCache = value
     return mixedValue
+end
+
+-- TODO: cache these results
+function ComputeCurrentSourceExpMult(source)
+    local currentPrestige = ZyruIncremental.Data.CurrentPrestige
+    if currentPrestige == 0 then
+        return 1
+    end
+    local multiplier = 1
+    for i, prestigeData in ZyruIncremental.Data.PrestigeData do
+        multiplier = multiplier * (prestigeData.ExperienceMulitpliers[source] or 1)
+    end
+    return multiplier
+end
+--[[
+    Computes next prestige's experience multipliers by given source.
+    FORMULA:
+    - New Multiplier = Math.pow([(50 + MaxPoints) / 50], 0.6) -- can vary exponent
+        - 10  points by god: 1.12x mult
+        - 25  points by god: 1.27x mult
+        - 50  points by god: 1.51x mult
+        - 100 points by god: 1.93x mult
+]]
+function ComputeNextSourceExpMult(source)
+    local prevMult = ComputeCurrentSourceExpMult(source)
+    -- GodData hardcoded
+    local maxPointsObtained = ZyruIncremental.Data.GodData[source].MaxPoints
+    local mult = math.pow((50 + maxPointsObtained) / 50, 0.6)
+
+    -- DebugPrint { Text = "Computing Next Source Mult: " .. source .. ": " .. tostring(mult) ", total mult: " .. tostring(prevMult * mult)  }
+
+    return mult
 end
